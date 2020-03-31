d3d15256ede41554e4599e5e5904f909dc8bab50
jxh_git <test@jxh_git.com>
2020-03-31 12:12:40 +0800

    jxh第三次提交数据 v1.1 博客列表、搜索实现

---
+++ src/main/java/com/edu/vo/EplatformQuery.java
@@ -0,0 +1,41 @@
+package com.edu.vo;
+
+/**
+ * @program: spring-boot-eplatform-01
+ * @description:
+ * @author: Mr.jia
+ * @date: 2020-03-30 16:45
+ **/
+public class EplatformQuery {
+
+    private String title;
+    private Long typeId;
+    private boolean recommend;
+
+    public EplatformQuery() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Long typeId) {
+        this.typeId = typeId;
+    }
+
+    public boolean isRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(boolean recommend) {
+        this.recommend = recommend;
+    }
+}
